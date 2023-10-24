6f088d7e6acd19df6dcb216aff75351b30f3cc4a
eluferiev <eluferiev@o-code.ru>
2023-10-24 18:14:33 +0400

    711

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -19,17 +19,25 @@ export const Expenses = ({ expenses }) => {
   //   filterInfoText = '2019, 2020, & 2021';
   // }
 
+  const filteredExpenses = expenses.filter(({ date }) => date.getFullYear().toString() === year);
+
   const selectYearHandler = (selectedYear) => {
     setYear(selectedYear);
   };
+
+  let expensesContent = <p>No expenses found.</p>;
+
+  if (filteredExpenses.length > 0) {
+    expensesContent = filteredExpenses.map((expense) => (
+      <ExpenseItem expense={expense} key={expense.id} />
+    ));
+  }
 
   return (
     <Card className='expenses'>
       <ExpensesFilter onSelectYear={selectYearHandler} year={year}/>
       {/* <p>Data for years {filterInfoText} is hidden.</p> */}
-      {expenses.map((expense) => (
-        <ExpenseItem expense={expense} key={expense.id} />
-      ))}
+      {expensesContent}
       {/* <ExpenseItem expense={expenses[0]}/>
       <ExpenseItem expense={expenses[1]}/>
       <ExpenseItem expense={expenses[1]}/> */}
